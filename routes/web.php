55bd31c7ca3ace4dca7c1e24997ea06a9ba97d9b
Jpmarasigan <jpmarasigan916@gmil.com>
2025-05-14 14:53:43 +0800

    new upload

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
+
 Route::get('/', function () {
     return redirect()->route('login');
 })->middleware('guest')->name('home');
@@ -35,33 +36,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
                 abort(403, 'Unauthorized role');
         }
     })->name('dashboard');
-
-//     // Reservations Routes
-//     Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations.index');
-//     Route::resource('reservations', ReservationController::class)->except('index');
-
-//     // Ticket Routes
-//     Route::get('/ticket', [TicketController::class, 'index'])->name('ticket.index');
-//     Route::resource('ticket', TicketController::class)->except('index');
-
-//     // ✅ Fixed route for updating ticket status
-//     Route::patch('/admin/tickets/{ticket}/status', [TicketController::class, 'updateStatus']);
-
-//     // Calendar Routes
-//     Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar.index');
-//     Route::resource('calendar', CalendarController::class)->except('index');
-
-//     // Logs Routes
-//     Route::get('/logs', [LogController::class, 'index'])->name('log.index');
-//     Route::resource('logs', LogController::class)->except('index');
-
-//     // Payment Routes
-//     Route::get('/payment', [PaymentController::class, 'index'])->name('payment.index');
-//     Route::resource('payment', PaymentController::class)->except('index');
-
-//     // User Management
-//     Route::get('/user', fn () => Inertia::render('Admin/User/Index'))->name('user.index');
-// 
 
  Route::middleware('role:admin')->group(function () {
         Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations.index');
